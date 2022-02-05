fafecb08eddc29c94d68dee17e06e273244a85cd
JunMalit <70352577+JunMalit@users.noreply.github.com>
2022-02-05 14:00:21 -0800

    Adding auton scripts to get cargo
    Adding auton scripts to get cargo at the end of the auton

---
+++ AutonBlueTurnTableGetCargo.java
@@ -0,0 +1,57 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.robot.ArmBot;
+import org.firstinspires.ftc.teamcode.robot.Robot;
+
+@Autonomous(name="AutonBlueTurnTableGetCargo", group="")
+//@Disabled
+
+public class AutonBlueTurnTableGetCargo extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException{
+            //Robot myRobot = new BareBonesBot(telemetry, hardwareMap);
+            //Robot myRobot = new SoftwareBot(telemetry, hardwareMap);
+            Robot myRobot = new ArmBot(telemetry, hardwareMap);
+
+            waitForStart();
+
+            int barCodeTier = myRobot.ScanBarCode();
+            myRobot.stopCameraStream();
+            double movePower = 0.6;
+            //myRobot.moveCollectorArm(1,0.4);
+//            sleep(10000);
+            //myRobot.moveCollectorArm(0);
+            myRobot.moveStraightInches(4, movePower);
+            myRobot.turnAngleDegrees(-93, movePower);
+            myRobot.moveStraightInches(14.5, 0.6);
+
+            myRobot.turnSpinner(0.8, -1);
+            sleep(3000);
+            myRobot.turnSpinner(0, -1);
+            myRobot.moveSidewaysInches(59, movePower);
+            myRobot.moveArmPosition(barCodeTier);
+            myRobot.turnAngleDegrees(-100, movePower);
+            myRobot.moveSidewaysInches(-47, movePower);
+            myRobot.turnArmSpinner(1, 0.36);
+            sleep(2000);
+            myRobot.turnArmSpinner(1, 0.0);
+            myRobot.moveSidewaysInches(-21, movePower);
+            myRobot.moveStraightInches(32, movePower);
+            myRobot.turnAngleDegrees(-90, movePower);
+            myRobot.moveStraightInches(54, 1.0);
+            myRobot.moveArmPosition(-1);
+            myRobot.turnAngleDegrees(90, movePower);
+            myRobot.turnArmSpinner(-1,0.6);
+            myRobot.moveStraightInches(9,movePower);
+            sleep(2000);
+            myRobot.turnArmSpinner(-1,0.0);
+            myRobot.moveStraightInches(-9,movePower);
+            myRobot.turnAngleDegrees(-180,movePower);
+            myRobot.saveHeading();
+    }
+}
+
---
+++ AutonRedTurnTableGetCargo.java
@@ -0,0 +1,57 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.robot.ArmBot;
+import org.firstinspires.ftc.teamcode.robot.Robot;
+
+@Autonomous(name="AutonRedTurnTableGetCargo", group="")
+//@Disabled
+
+public class AutonRedTurnTableGetCargo extends LinearOpMode {
+
+    @Override
+    public void runOpMode() {
+            //Robot myRobot = new BareBonesBot(telemetry, hardwareMap);
+            //Robot myRobot = new SoftwareBot(telemetry, hardwareMap);
+            Robot myRobot = new ArmBot(telemetry, hardwareMap);
+
+            waitForStart();
+
+            int barCodeTier = myRobot.ScanBarCode();
+
+//            myRobot.moveCollectorArm(1,1.0);
+//            sleep(2000);
+//            myRobot.moveCollectorArm(0, 0.4);
+
+            double movePower = 0.6;
+            myRobot.moveStraightInches(5, movePower);
+            myRobot.turnAngleDegrees(185, movePower);
+            myRobot.moveSidewaysInches(-32, movePower);
+            myRobot.turnSpinner(0.8, 1);
+            sleep(3000);
+            myRobot.turnSpinner(0, 1);
+            myRobot.moveStraightInches(-54, movePower);
+            myRobot.moveArmPosition(barCodeTier);
+            myRobot.moveSidewaysInches(50, movePower);
+            myRobot.turnArmSpinner(1, 0.4);
+            sleep(2000);
+            myRobot.turnArmSpinner(1, 0.0);
+            myRobot.moveStraightInches(-2, movePower);
+            myRobot.moveSidewaysInches(26, movePower);
+            myRobot.moveStraightInches(31, movePower);
+            myRobot.turnAngleDegrees(98, movePower);
+            myRobot.moveStraightInches(55, 1.0);
+            myRobot.moveArmPosition(-1);
+            myRobot.turnAngleDegrees(-95, movePower);
+            myRobot.turnArmSpinner(-1,0.6);
+            myRobot.moveStraightInches(9,movePower);
+            sleep(2000);
+            myRobot.turnArmSpinner(-1,0.0);
+            myRobot.moveStraightInches(-5,movePower);
+            myRobot.turnAngleDegrees(185,movePower);
+            myRobot.saveHeading();
+    }
+}
+
